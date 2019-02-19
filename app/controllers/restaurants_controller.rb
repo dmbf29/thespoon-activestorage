ff078a6df8e8e60186c9394a1dcf9cf610aff2c7
Douglas Berkley <douglasmberkley@gmail.com>
2019-02-19 16:16:12 +0900

    rails day 2 lecture

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,56 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @restaurants = Restaurant.all
+    # render index.html.erb
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+      # new.html.erb
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :stars)
+  end
+end
+
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # CRUD
+  # HTTP-VERB PATH to: "CONTROLLER#ACTION", AS: PREFIX
+
+  # # index
+  # get 'restaurants', to: 'restaurants#index'
+
+  # # show
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  # # create
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+
+  # # update
+  # get 'restaurants/:id/edit', to: 'restaurants#edit', as: :restaurants_edit
+  # patch 'restaurants/:id', to: 'restaurants#update'
+
+  # # destroy
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
+
+  resources :restaurants
 end
+
+
+# link_to restaurants_path
+
+
